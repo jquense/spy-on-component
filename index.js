821712fc6f2969a94d00096b2520aa3ec4f3b5cb
Jason Quense <monastic.panic@gmail.com>
2018-03-30 12:36:03 -0400

    feat: Support react 16.3 new lifecycle hooks

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-
 var LIFECYCLE_HOOKS = {
   componentWillMount: true,
   componentDidMount: true,
   componentWillReceiveProps: true,
+  getSnapshotBeforeUpdate: true,
   shouldComponentUpdate: true,
   componentWillUpdate: true,
   componentDidUpdate: true,
   componentWillUnmount: true,
 }
 
-function wrap(base, method) {
-  var before = true;
+var STATIC_HOOKS = {
+  getDerivedStateFromProps: true,
+}
+
+function wrap(base, method, isStatic) {
+  var before = true
 
   if (Array.isArray(method)) {
     before = method[0] !== 'after'
     method = method[1]
   }
 
-  if (!base)
-    return method;
+  if (!base) return method
 
   return function wrappedLifecyclehook() {
-    before && method.apply(this, arguments)
-    base.apply(this, arguments)
-    !before && method.apply(this, arguments)
+    const ctx = isStatic ? null : this
+    before && method.apply(ctx, arguments)
+    base.apply(ctx, arguments)
+    !before && method.apply(ctx, arguments)
   }
 }
 
 module.exports = function spyOnComponent(component, hooks) {
-  var originals = Object.create(null);
+  var originals = Object.create(null)
 
-  for (var key in hooks) if (LIFECYCLE_HOOKS[key])
-    component[key] = wrap(
-      originals[key] = component[key],
-      hooks[key]
-    )
+  for (var key in hooks)
+    if (STATIC_HOOKS[key])
+      component.constructor[key] = wrap(
+        (originals[key] = component.constructor[key]),
+        hooks[key],
+        true
+      )
+
+  for (var key in hooks)
+    if (LIFECYCLE_HOOKS[key])
+      component[key] = wrap((originals[key] = component[key]), hooks[key])
 
   return function reset(key) {
-    if (key && {}.hasOwnProperty.call(originals, key))
-      component[key] = originals[key]
-    else for (var key in originals)
-      component[key] = originals[key]
+    const subject = STATIC_HOOKS[key] ? component.constructor : component
+
+    if (key && key in originals) subject[key] = originals[key]
+    else for (var key in originals) subject[key] = originals[key]
   }
 }
 
 module.exports.mixin = function mixinIntoComponent(componentClass, hooks) {
   spyOnComponent(componentClass.prototype, hooks)
-  return componentClass;
+  return componentClass
 }
--- test.js
+++ test.js
@@ -1,18 +1,29 @@
-"use strict";
-let assert = require('assert');
-let spyOnComponent = require('./');
+'use strict'
+let assert = require('assert')
+let spyOnComponent = require('./')
 
-let innerCalled = false;
-let called = false;
+let staticCalled = false
+let staticInnerCalled = false
+let innerCalled = false
+let called = false
 
-
-function foo() { innerCalled = true }
-
-let component = {
-  componentWillMount: foo,
+function foo() {
+  innerCalled = true
 }
 
+class Component {
+  static getDerivedStateFromProps() {
+    staticInnerCalled = true
+  }
+}
+
+Component.prototype.componentWillMount = foo
+const component = new Component()
+
 let reset = spyOnComponent(component, {
+  getDerivedStateFromProps() {
+    staticCalled = true
+  },
   componentWillMount() {
     called = true
   },
@@ -20,25 +31,37 @@ let reset = spyOnComponent(component, {
 })
 
 assert(true, 'it didnt have runtime errors')
-
+//
+//
 ;((/* it should wrap methods */) => {
   component.componentWillMount()
 
   assert(called)
   assert(innerCalled)
 })()
+//
+//
+;((/* it should wrap static methods */) => {
+  Component.getDerivedStateFromProps()
 
+  assert(staticCalled)
+  assert(staticInnerCalled)
+})()
+//
+//
 ;((/* it should only wrap when needed */) => {
   assert.equal(component.componentDidMount, foo)
 })()
-
+//
+//
 ;((/* it should reset by key */) => {
   reset('componentDidMount')
 
   assert.equal(component.componentDidMount, undefined)
   assert.notEqual(component.componentWillMount, foo)
 })()
-
+//
+//
 ;((/* it should reset */) => {
   reset()
   assert.equal(component.componentWillMount, foo)
